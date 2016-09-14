4ca918d42102805317446cd3c6d72ee38e8a1808
Furvar <Furvar@users.noreply.github.com>
2016-09-14 21:05:25 +0200

    Add files via upload

--- be/Vilevar/WolvCoins/WolvCoins.java
+++ be/Vilevar/WolvCoins/WolvCoins.java
@@ -7,14 +7,21 @@ import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import be.Vilevar.WolvCoins.Commands.Coins;
+import be.Vilevar.WolvCoins.Events.EventsManager;
+import be.Vilevar.WolvCoins.Jobs.Jobs;
 
-public class WolvCoins extends JavaPlugin implements Listener {
+public class WolvCoins extends JavaPlugin {
 
+	public HashMap<UUID, Jobs> jobs = new HashMap<>();
+	public HashMap<UUID, Integer> level = new HashMap<>();
+	public HashMap<UUID, Double> miniLvl = new HashMap<>();
+	
+	//PAS OUBLIER DE METTRE LA PERMISSION A LA COMMANDE /COINS SET <JOUEUR> <NB DE COINS>
+	
+	//premium
 	public HashMap<UUID, Integer> premium = new HashMap<>();
 	public HashMap<UUID, Integer> premium1 = new HashMap<>();
 	public HashMap<UUID, Integer> premium2 = new HashMap<>();
@@ -27,11 +34,12 @@ public class WolvCoins extends JavaPlugin implements Listener {
 	public HashMap<UUID, Integer> premium9 = new HashMap<>();
 	public HashMap<UUID, Integer> premium10 = new HashMap<>();
 
+	//best coins
 	public double meilleurCW = 1;
 	public String meilleurPlayer = "";
 
+	//economy
 	public static Economy economy = null;
-
 	public static boolean setupEconomy() {
 		RegisteredServiceProvider<Economy> economyProvider = WolvCoins.getPlugin(WolvCoins.class).getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
 		if (economyProvider != null) {
@@ -40,15 +48,21 @@ public class WolvCoins extends JavaPlugin implements Listener {
 		return (economy != null);
 	}
 
+	public static WolvCoins instance;
+	
+	public static WolvCoins getInstance(){
+		return instance;
+	}
+	
+	@Override
 	public void onEnable() {
-		Bukkit.getPluginManager().registerEvents(this, this);
-
-		// commands
-		getCommand("coins").setExecutor(new Coins(this));
+		super.onEnable();
+		instance = this;
+		new EventsManager(this).registerEvents();
+		
 		// addCoins
 		long temps = 20 * 60;
-		Bukkit.getScheduler().scheduleSyncRepeatingTask(this,
-				new Runnable() {
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
 
 					@Override
 					public void run() {
